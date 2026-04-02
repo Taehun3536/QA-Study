ca7ac9a80495335324da94ec9f33a2a9bda2ef9a
Taehun3536 <hg1282rnjs@gmail.com>
2026-04-02 16:04:01 +0900

    Model, dto 수정

--- src/main/java/com/ohgiraffers/qa/dto/UpdatePostRequest.java
+++ src/main/java/com/ohgiraffers/qa/dto/UpdatePostRequest.java
@@ -1,11 +1,13 @@
 package com.ohgiraffers.qa.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record UpdatePostRequest(
-        @NotBlank
+        @NotBlank(message = "제목은 필수입니다.")
+        @Size(max = 100, message = "제목은 100자를 초과할 수 없습니다.")
         String title,
-        @NotBlank
+        @NotBlank(message = "내용은 필수입니다.")
         String content
 ) {
 }
--- src/main/java/com/ohgiraffers/qa/model/Board.java
+++ src/main/java/com/ohgiraffers/qa/model/Board.java
@@ -1,9 +1,6 @@
 package com.ohgiraffers.qa.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +17,10 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long boardId;
 
+    @Column(length = 100)
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     private Long writerId;
